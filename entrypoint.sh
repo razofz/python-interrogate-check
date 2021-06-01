1efdede750f1787825cdda614e0c1007fa8f450e
razofz <rasmus.olofzon@gmail.com>
2021-06-01 18:20:31 +0200

    attempt at supporting more interrogate flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,19 @@ set -eax
 
 interrogate --version
 
-if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
-    interrogate --fail-under $2 --generate-badge $3 $1
+FLAG=''
+if [[ ${4} == true ]]; then FLAG=$FLAG'n' ; fi
+if [[ ${5} == true ]]; then FLAG=$FLAG'p' ; fi
+if [[ ${6} == true ]]; then FLAG=$FLAG's' ; fi
+if [[ ${7} == true ]]; then FLAG=$FLAG'm' ; fi
+if [[ ${FLAG} ]]; then FLAG=" -${FLAG}" ; fi
+CMD="interrogate --fail-under $2 --generate-badge $3 $1 $FLAG"
+
+if $CMD | grep -q 'PASSED'; then
+    $CMD
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $1
+    $CMD
     exit 1
 fi
+
